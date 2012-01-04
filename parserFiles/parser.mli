8896ba1b2882921a91730e30ac118ca954740c46
mukku.daisuki@gmail.com <mukku.daisuki@gmail.com@4c1d2e2c-7561-eade-ff35-5f918fd1767b>
2012-01-04 21:41:14 +0000

    modu³ do liczenia rozmiaru maszyny

--- parserFiles/parser.mli
+++ parserFiles/parser.mli
@@ -1,3 +1,15 @@
+type newState = WildCard | NewState of int
+		and variable = Var of string		
+		and cond = Equals of variable * int | And of cond list | 
+                   Or of cond list
+		and statement = If of cond * statement * elseIf list * statement | 
+                 Decision of newState * string | 
+		 Case of variable * arm list * statement
+		and arm = Arm of int list * statement
+		and elseIf = ElseIf of cond * statement
+		and rule = Rule of int list * statement
+		and character = Character of rule list
+
 type token =
   | INT of (int)
   | STRING of (string)
